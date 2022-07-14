11b1f6668ae96a3638db6c887ba3daaf4093d9fa
tranngminhdao <107382512+tranngminhdao@users.noreply.github.com>
2022-07-14 22:41:59 +0700

    Complete code structure
    - Adding folder structure
    - Apply SCSS and BEM

---
+++ src/layout/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div>Home</div>
+    </div>
+  );
+};
+
+export default App;
