1c20d59307e1c0626b692745e621bb7768790507
JustTory <johnnynathany@gmail.com>
2022-07-16 19:11:23 +0700

    added service selection feature

--- src/component/bookingStep/index.jsx
+++ src/component/bookingStep/index.jsx
@@ -3,12 +3,14 @@ import "./bookingForm.scss";
 import BookingHeader from "./bookingHeader";
 import BookingFooter from "./bookingFooter";
 import StepInfo from "./step/stepInfo";
+import StepService from "./step/stepService";
 import { validateStepOne } from "./validators";
 
 const stepper = ["Information", "Services", "DateTime"];
 const BookingStep = () => {
   const [activeStep, setActiveStep] = useState(0);
   const [information, setInformation] = useState();
+  const [selectedServices, setSelectedServices] = useState([]);
 
   const handlePrev = () => {
     if (activeStep > 0) setActiveStep(activeStep - 1);
@@ -22,6 +24,12 @@ const BookingStep = () => {
         alert("Please input all field");
         return;
       }
+    } else if (activeStep === 1) {
+      if (selectedServices.length === 0) {
+        alert("Please select at least 1 service");
+        return;
+      }
+      console.log("selected services:", selectedServices);
     }
     if (activeStep < stepper.length - 1)
       setActiveStep(activeStep + 1);
@@ -34,7 +42,12 @@ const BookingStep = () => {
           <StepInfo data={information} setData={setInformation} />
         );
       case 1:
-        return <p>Step 2</p>;
+        return (
+          <StepService
+            selectedServices={selectedServices}
+            setSelectedServices={setSelectedServices}
+          />
+        );
       case 2:
         return <p>Step 3</p>;
       default:
