7df11003965c3d42b4f732b834f0a383a2b1eeb1
JustTory <johnnynathany@gmail.com>
2022-07-27 16:14:49 +0700

    added statistic

--- src/constants/icon.jsx
+++ src/constants/icon.jsx
@@ -4,5 +4,16 @@ import categoriesIc from "../assets/icons/categories.png";
 import servicesIc from "../assets/icons/services.png";
 import clientsIc from "../assets/icons/clients.png";
 import employeesIc from "../assets/icons/employees.png";
+import notiIc from "../assets/icons/noti.png";
+import avatarIc from "../assets/icons/avatar.png";
 
-export { logoIc, overviewIc, categoriesIc, servicesIc, clientsIc, employeesIc };
+export {
+  logoIc,
+  overviewIc,
+  categoriesIc,
+  servicesIc,
+  clientsIc,
+  employeesIc,
+  notiIc,
+  avatarIc,
+};
--- src/layout/dashboard/index.jsx
+++ src/layout/dashboard/index.jsx
@@ -1,12 +1,65 @@
-import LeftNav from "component/leftNav";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./dashboard.scss";
+import LeftNav from "component/leftNav/leftNav";
+import StatBox from "component/statBox/statBox";
+import { notiIc, avatarIc } from "constants/icon";
+
+const statistic = [
+  {
+    title: "Ongoing",
+    value: 60,
+  },
+
+  {
+    title: "Overdue",
+    value: 16,
+  },
+  {
+    title: "Cancelled",
+    value: 43,
+  },
+  {
+    title: "Total",
+    value: 119,
+  },
+];
 
 const Dashboard = () => {
+  const [stats, setStats] = useState([]);
+
+  useEffect(() => {
+    // call api here
+    setStats(statistic);
+  }, []);
+
   return (
-    <div className="dashboard">
+    <div className="main">
       <LeftNav />
-      <div className="dashboard__content">
-        <p>Admin Dashboard</p>
+
+      <div className="dashboard">
+        <div className="dashboard__header">
+          <p className="title">Overview</p>
+
+          <div className="control">
+            <img className="image" src={notiIc} alt="noti" />
+
+            <div className="control__user">
+              <p className="control__user__name">Administrator</p>
+              <img className="image" src={avatarIc} alt="avatar" />
+            </div>
+          </div>
+        </div>
+
+        <div className="dashboard__statistic">
+          {stats &&
+            stats.map((stat) => (
+              <StatBox title={stat.title} value={stat.value} />
+            ))}
+        </div>
+
+        <div className="dashboard__booking">
+          <p>booking</p>
+        </div>
       </div>
     </div>
   );
